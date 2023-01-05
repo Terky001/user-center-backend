ee9650a2cf9f38f97419066b7be640e3ac794fe8
Terky <terky1019@163.com>
2023-01-05 22:07:23 +0800

    1.5 刷新前

--- src/main/java/com/terky/usercenter/service/UserService.java
+++ src/main/java/com/terky/usercenter/service/UserService.java
@@ -10,4 +10,12 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface UserService extends IService<User> {
 
+    /**
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 用户id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 }
--- src/main/java/com/terky/usercenter/service/impl/UserServiceImpl.java
+++ src/main/java/com/terky/usercenter/service/impl/UserServiceImpl.java
@@ -1,10 +1,17 @@
 package com.terky.usercenter.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.terky.usercenter.model.User;
 import com.terky.usercenter.service.UserService;
 import com.terky.usercenter.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
 * @author www
@@ -15,6 +22,48 @@ import org.springframework.stereotype.Service;
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
     implements UserService{
 
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        //校验
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword))
+            return -1;
+        if (userAccount.length() < 4)
+            return -1;
+        if (userPassword.length() < 8 || checkPassword.length() < 8)
+            return -1;
+
+
+        //账户不能包含特殊字符  正则
+        String validPattern = "[\\\\u00A0\\\\s\\\"`~!@#$%^&*()+=|{}':;',\\\\[\\\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (matcher.find())
+            return -1;
+
+        //密码和校验密码相同
+        if (!userPassword.equals(checkPassword))
+            return -1;
+
+        //账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount",userAccount);
+        long count = this.count(queryWrapper);
+        if (count > 0)
+            return -1;
+
+        //加密密码
+        final String SALT = "terky";
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+
+        //插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (!saveResult) //避免拆箱错误
+            return -1;
+
+        return user.getId();
+    }
 }
 
 
